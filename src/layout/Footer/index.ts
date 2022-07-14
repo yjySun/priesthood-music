7718482a72e6a60a23135baf5a463f3cb4d5c727
yaojiyuan <yaojiyuan@shinow.com>
2022-07-14 22:40:33 +0800

    完成整体布局

---
+++ src/layout/Footer/index.ts
@@ -0,0 +1,3 @@
+import PlayFooter from './index.vue'
+
+export { PlayFooter }
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,13 +1,58 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
+  <div class="layout">
+    <el-scrollbar>
+      <div class="common-layout">
+        <el-container style="height: 100%">
+          <el-header>Header</el-header>
+          <el-container>
+            <el-aside>
+              <AsideMenu />
+            </el-aside>
+            <el-main>
+              <MainView />
+            </el-main>
+          </el-container>
+          <el-footer>
+            <PlayFooter />
+          </el-footer>
+        </el-container>
+      </div>
+    </el-scrollbar>
   </div>
 </template>
-<script lang="ts" setup></script>
-<style scoped></style>
+<script lang="ts" setup>
+  import { AsideMenu } from './Menu/index'
+  import { PlayFooter } from './Footer/index'
+  import { MainView } from './Main/index'
+</script>
+<style lang="scss">
+  .layout {
+    height: 100%;
+
+    .el-header {
+      background-color: #ec4141;
+    }
+
+    .el-aside {
+      background-color: rgb(76, 0, 255);
+    }
+
+    .el-footer {
+      background-color: green;
+    }
+
+    .common-layout {
+      height: 100%;
+      min-width: 1400px;
+    }
+
+    .el-scrollbar {
+      width: 100%;
+      height: 100%;
+
+      .el-scrollbar__wrap.el-scrollbar__wrap--hidden-default .el-scrollbar__view {
+        height: 100% !important;
+      }
+    }
+  }
+</style>
